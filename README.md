bfb40c42afcb2215deee559aa740da666c63a7f8
maurofmferrao <maurofmferrao@gmail.com>
2020-10-09 16:09:07 +0100

    Library first version

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
-# xibo-interactive-control
+# Xibo Interactive Control
+
+This library is meant to be used with Xibo () Digital Signage Players, and handles the manupilation of ...
+
+## Instalation
+
+Copy src/xibo-interactive-control.js to the module folder in the CMS or add to any HTML file.
+
+### Add to a CMS module
+
+Library JS file needs to be added to the getResource() method in the module's PHP file.
+
+Example:
+
+```php
+$javacriptToAdd = '<script type="text/javascript" src="' . $this->getResourceUrl('xibo-interactive-control.js') . '"></script>';
+$data['javaScript'] = $javacriptToAdd;
+return $this->renderTemplate($data);
+```
+
+## Library usage
+
+### Config Library
+
+Use to update the default parameters used to connect with the server. By default the requests are made locally ( '/info' for example ), but by changing the following parameters we can connect to a remote server
+
+```javascript
+xiboIC.config(libOptions)
+```
+
+- libOptions.protocol
+- libOptions.hostname
+- libOptions.port
+- libOptions.headers: Array of headers in the format “key: value” ( e.g. _[{ key: 'Content-Type', value: 'application/x-www-form-urlencoded' }]_ )
+
+#### All the following methods will have a **reqOptions** parameter with the following options:
+
+- reqOptions.done: callback to run when the request is successful
+- reqOptions.progress: callback to run during the request’s progress
+- reqOptions.error: callback to run when the request fails
+- reqOptions.waitForLoad: Wait for the page to load to run this method
+
+### Get Player Info
+
+Get information about the player
+
+```javascript
+xiboIC.info(reqOptions)
+```
+
+### Trigger action/web-hook
+
+Trigger action in the parent player
+
+```javascript
+xiboIC.trigger(code, reqOptions)
+```
+
+- **code**: trigger code for the web-hook that initiates an action in the parent player
+
+### Add function to the queue
+
+Add a function to a queue so it can be ran later
+
+```javascript
+xiboIC.addToQueue(callback, ...args)
+```
+
+- **callback**: function definition
+- **args**: comma separated arguments for the target function
+
+### Run full queue
+
+Run all the pending function in the queue ( and remove them )
+
+```javascript
+xiboIC.runQueue()
+```
+
+### Set as visible
+
+Set widget state as visible and run **runQueue()**
+
+```javascript
+xiboIC.setVisible()
+```
+
+### Expire
+
+Expire current widget
+
+```javascript
+xiboIC.expireNow(reqOptions)
+```
+
+### Extend duration
+
+Extend widget duration using a given value
+
+```javascript
+xiboIC.extendWidgetDuration(extend, reqOptions)
+```
+
+### Set duration
+
+Set widget duration to a given value
+
+```javascript
+xiboIC.setWidgetDuration(duration, reqOptions)
+```
---
+++ src/xibo-interactive-control.js
@@ -0,0 +1,246 @@
+/**
+ * Copyright (C) 2020 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+const xiboIC = (function() {
+    'use strict';
+
+    // Private vars
+    const _lib = {
+        protocol: '', // default protocol 
+        hostName: '', // default URL 
+        port: '', // default PORT 
+        headers: [], // Default headers 
+        timelimit: 5000, // timelimit in milliseconds
+        callbackQueue : [],
+
+        getOriginURL: function() {
+            if (this.protocol != '' && this.hostName != '') {
+                return this.protocol + '://' + this.hostName + ((this.port != '') ? ':' + this.port : '');
+            }
+            return '';
+        },
+
+        makeRequest: function(path, {type, headers, data, done, progress, error} = {}) {
+            const self = this;
+
+            // Check if we are in preview mode
+            let playerAction;
+            const isPreview = function() {
+                // For the widget preview in viewer
+                if(typeof window.parent.lD != 'undefined') {
+                    playerAction = window.parent.lD.playerAction;
+                    return true;
+                }
+
+                // For layout preview in viewer
+                if(typeof window.parent.parent.lD != 'undefined') {
+                    playerAction = window.parent.parent.lD.playerAction;
+                    return true;
+                }
+            }();
+
+            // Preview/Handle action in viewer if exists
+            if(isPreview && playerAction != undefined) {
+                playerAction(path, data);
+                return;
+            }
+            
+            const urlToUse = self.getOriginURL() + path;
+            const typeToUse = (type) ? type : 'GET';
+            const reqHeaders = (headers) ? headers : self.headers;
+
+            // Init AJAX
+            let xhr = new XMLHttpRequest();
+            xhr.timeout = self.timelimit;
+
+            xhr.open(typeToUse, urlToUse, true);
+
+            // Set headers
+            if(type == 'POST') {
+                xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+            }
+
+            reqHeaders.forEach(header => {
+                xhr.setRequestHeader(header.key, header.value);
+            });
+
+            // Append data
+            let newData = null;
+            if (typeof(data) == "object") {
+                newData = JSON.stringify(data);
+            }
+
+            // On load complete
+            xhr.onload = function() {
+                if (typeof(done) == "function") {
+                    done(this);
+                }
+            };
+
+            // On error
+            if (typeof(error) == "function") {
+                xhr.onerror = error;
+            }
+
+            // On progress
+            if (typeof(progress) == "function") {
+                xhr.onprogress = progress;
+            }
+
+            // Send!
+            xhr.send(newData);
+        },
+    };
+
+    // Public library
+    const mainLib = {
+        isVisible: true, // Widget visibility on the player 
+
+        checkVisible: function() { // Check if the widget is hidden or visible
+            const urlParams = new URLSearchParams(location.search);
+            mainLib.isVisible = (urlParams.get("visible")) ? (urlParams.get("visible") == 1) : true;
+            return mainLib.isVisible;
+        },
+
+        /**
+         * Configure the library options
+         * @param  {Object} options
+         * @param  {string} options.hostName
+         * @param  {string} options.port
+         * @param  {Object[]} options.headers
+         * @param  {string} options.headers.key
+         * @param  {string} options.headers.value
+         * @param  {string} options.protocol
+         */
+        config: function({ hostName, port, headers, protocol } = {}) {
+            // Initialise custom request params
+            _lib.hostName = hostName ? hostName : _lib.hostName;
+            _lib.port = port ? port : _lib.port;
+            _lib.headers = headers ? headers : _lib.headers;
+            _lib.protocol = protocol ? protocol : _lib.protocol;
+        },
+
+        // Get player info
+        info: function({ done, progress, error } = {}) {
+            _lib.makeRequest(
+                '/info',
+                {
+                    done: done,
+                    progress: progress,
+                    error: error
+                }
+            );
+        },
+        
+        // Trigger a predefined action
+        trigger(code, { done, progress, error } = {}) {
+            _lib.makeRequest(
+                '/trigger',
+                {
+                    type: 'POST',
+                    data: {
+                        trigger: code
+                    },
+                    done: done,
+                    progress: progress,
+                    error: error
+                }
+            );
+        },
+
+        // Expire widget
+        expireNow({ done, progress, error } = {}) {
+            _lib.makeRequest(
+                '/expirenow',
+                {
+                    type: 'POST',
+                    done: done,
+                    progress: progress,
+                    error: error
+                }
+            );
+        },
+        
+        // Extend widget duration
+        extendWidgetDuration(extend, { done, progress, error } = {}) {
+            _lib.makeRequest(
+                '/extendduration',
+                {
+                    type: 'POST',
+                    data: {
+                        extend: extend
+                    },
+                    done: done,
+                    progress: progress,
+                    error: error
+                }
+            );
+        },
+        
+        // Set widget duration
+        setWidgetDuration(duration, { done, progress, error } = {}) {
+            _lib.makeRequest(
+                '/setduration',
+                {
+                    type: 'POST',
+                    data: {
+                        duration: duration
+                    },
+                    done: done,
+                    progress: progress,
+                    error: error
+                }
+            );
+        },
+
+        // Add callback function to the queue
+        addToQueue(callback, ...args) {
+            if(typeof callback != 'function') {
+                console.error('Invalid callback function');
+            }
+
+            _lib.callbackQueue.push({
+                callback: callback,
+                arguments: args
+            });
+        },
+
+         // Run promised functions in queue
+        runQueue() {
+            _lib.callbackQueue.forEach((element) => {
+                element.callback.apply(_lib, element.arguments);
+            });
+
+            // Empty queue
+            _lib.callbackQueue = [];
+        },
+
+        // Set visible and run queue
+        setVisible() {
+            this.isVisible = true;
+            this.runQueue();
+        }
+    };
+
+    // Check visibility on load
+    mainLib.checkVisible();
+    
+    return mainLib;
+})();
